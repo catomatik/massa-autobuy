b7688ffe05463dc3d0ae42ed5b2b1dc20e941f0d
Alex <catomatik@gmail.com>
2022-12-18 15:54:35 +0100

    Updated for massa 17.2, add telegram notification

--- autobuy.sh
+++ autobuy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Autobuy script for Massa 15.x
+# Autobuy script for Massa 17.2
 # Add to crontab for every minute
 # crontab -e
 #
@@ -7,7 +7,10 @@
 #
 # 2022 (c) catomatik@gmail.com
 
-PASSWORD=""
+# Replace with your data
+TELEGRAM_BOT_ID="BOT_ID"
+TELEGRAM_CHAT_ID="CHAT_ID"
+PASSWORD="NODE_PASSWORD"
 
 cd $HOME/massa/massa-client
 
@@ -19,15 +22,18 @@ active_rolls=$( ./massa-client -p $PASSWORD -j wallet_info | jq -r '.[].address_
 final_rolls=$( ./massa-client -p $PASSWORD -j wallet_info | jq -r '.[].address_info.final_rolls')
 candidate_rolls=$( ./massa-client -p $PASSWORD -j wallet_info | jq -r '.[].address_info.candidate_rolls')
 
-echo Address: $massa_wallet_address 
+echo Address: $massa_wallet_address
 echo Balance: $balance
 echo Active rolls: $active_rolls
 echo Final rolls: $final_rolls
 echo Candidate rolls: $candidate_rolls
 
-if [ $int_balance -gt "99" ]  && [ $candidate_rolls -lt "1" ]; then
-	echo No active rolls! Trying to buy a roll..
-	echo $(date) No rolls, trying to buy.. >> $HOME/autobuy.log
-	echo $(date) Ballance $balance, Active: $active_rolls, Final: $final_rolls, Candidate: $candidate_rolls >> $HOME/autobuy.log
-	./massa-client buy_rolls $massa_wallet_address 1 0
+if [ $int_balance -ge "100" ]; then
+        echo No active rolls! Trying to buy a roll..
+        echo $(date) No rolls, trying to buy.. >> $HOME/autobuy.log
+        echo $(date) Ballance $balance, Active: $active_rolls, Final: $final_rolls, Candidate: $candidate_rolls >> $HOME/autobuy.log
+        let "rolls_to_buy = $int_balance / 100"
+        echo "Rolls to buy: $rolls_to_buy"
+        ./massa-client -p $PASSWORD buy_rolls $massa_wallet_address $rolls_to_buy 0
+        curl -s "https://api.telegram.org/bot$TELEGRAM_BOT_ID/sendMessage?parse_mode=HTML&chat_id=$TELEGRAM_CHAT_ID&text=<b>$HOSTNAME</b>%0ANew massa roll bought: $rolls_to_buy"
 fi
